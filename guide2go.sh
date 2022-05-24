2279164474751e16c4866671d72c742e15c52368
kegbeach <81620868+kegbeach@users.noreply.github.com>
2022-05-23 22:33:55 -0400

    add XMLTV backup support

--- guide2go.sh
+++ guide2go.sh
@@ -10,12 +10,15 @@
 # Set $RETRYDELAY to the time between retry loops eg: 60s
 # Set $ALERTEMAIL to the administrator e-mail
 # Set $SERVERNAME to the hostname of this server
+# Set $MAXBACKUPS to the number of backups to keep
+# Set $XMLWORK to the guide2go XMLTV file
+# Set $JSONWORK to the guide2go cache file
 # Leave $LOOPCOUNTER unchanged
 #
 # Set $DEBUG=1 to enable debugging / 0 to turn off
 #
 
-DEBUG=1
+DEBUG=0
 BASEDIR=/path/to/guide2go
 CONF=guide2go_config.yaml
 LOGFILE=guide2go.log
@@ -24,7 +27,108 @@ RETRIES=5
 RETRYDELAY=60s
 ALERTEMAIL=your@email.com
 SERVERNAME=your.server.com
+MAXBACKUPS=5
+XMLWORK=guide2go-schedule.xml
+JSONWORK=guide2go-cache.json
 LOOPCOUNTER=1
+
+checkNumFiles() {
+    XMLCOUNT=$(find $BASEDIR/backup -type f -name "$XMLWORK-*" -printf '%f\n' | wc -l)
+    JSONCOUNT=$(find $BASEDIR/backup -type f -name "$JSONWORK-*" -printf '%f\n' | wc -l)
+}
+
+checkMD5() {
+    XMLFILE=$(find $BASEDIR/backup -type f -name "$XMLWORK-*" -printf '%T+ %f\n' | sort -r | cut -d' ' -f2 | sed -n '1 p')
+    JSONFILE=$(find $BASEDIR/backup -type f -name "$JSONWORK-*" -printf '%T+ %f\n' | sort -r | cut -d' ' -f2 | sed -n '1 p')
+
+    if [[ -f $BASEDIR/backup/$XMLFILE && ! -z $XMLFILE && -f $BASEDIR/$XMLWORK ]]; then
+        MD5XMLBAK=$(md5sum $BASEDIR/backup/$XMLFILE | cut -d' ' -f1)
+        MD5XMLWORK=$(md5sum $BASEDIR/$XMLWORK | cut -d' ' -f1)
+
+        if [[ $MD5XMLBAK == $MD5XMLWORK ]]; then
+            MD5XML=1
+        else
+            MD5XML=0
+        fi
+    fi
+
+    if [[ -f $BASEDIR/backup/$JSONFILE && ! -z $JSONFILE && -f $BASEDIR/$JSONWORK ]]; then
+        MD5JSONBAK=$(md5sum $BASEDIR/backup/$JSONFILE | cut -d' ' -f1)
+        MD5JSONWORK=$(md5sum $BASEDIR/$JSONWORK | cut -d' ' -f1)
+
+        if [[ $MD5JSONBAK == $MD5JSONWORK ]]; then
+            MD5JSON=1
+        else
+            MD5JSON=0
+        fi
+    fi
+}
+
+trimOldFiles() {
+    if checkNumFiles; then
+        if [[ $XMLCOUNT -gt $MAXBACKUPS ]]; then
+            x=$((XMLCOUNT-MAXBACKUPS))
+            for (( c=0; c<$x; c++ )); do
+                XMLFILE=$(find $BASEDIR/backup -type f -name "$XMLWORK-*" -printf '%T+ %f\n' | sort | cut -d' ' -f2 | sed -n '1 p')
+
+                rm -rf $BASEDIR/backup/$XMLFILE
+            done
+        fi
+
+        if [[ $JSONCOUNT -gt $MAXBACKUPS ]]; then
+            x=$((JSONCOUNT-MAXBACKUPS))
+            for (( c=0; c<$x; c++ )); do
+                JSONFILE=$(find $BASEDIR/backup -type f -name "$JSONWORK-*" -printf '%T+ %f\n' | sort | cut -d' ' -f2 | sed -n '1 p')
+
+                rm -rf $BASEDIR/backup/$JSONFILE
+            done
+        fi
+    fi
+}
+
+addBackup() {
+    if checkMD5; then
+        if [[ $MD5XML -eq 1 ]]; then
+            :
+        else
+            if [[ -f $BASEDIR/$XMLWORK ]]; then
+                cp $BASEDIR/$XMLWORK $BASEDIR/backup/$XMLWORK-$(date +%m-%d-%Y-%T)
+            fi
+        fi
+
+        if [[ $MD5JSON -eq 1 ]]; then
+            :
+        else
+            if [[ -f $BASEDIR/$JSONWORK ]]; then
+                cp $BASEDIR/$JSONWORK $BASEDIR/backup/$JSONWORK-$(date +%m-%d-%Y-%T)
+            fi
+        fi
+    fi
+}
+
+restoreBackup() {
+    if checkMD5; then
+        if [[ $MD5XML -eq 1 ]]; then
+            :
+	else
+            XMLFILE=$(find $BASEDIR/backup -type f -name "$XMLWORK-*" -printf '%T+ %f\n' | sort -r | cut -d' ' -f2 | sed -n '1 p')
+
+            if [[ -f $BASEDIR/backup/$XMLFILE && ! -z $XMLFILE ]]; then
+                cp -f $BASEDIR/backup/$XMLFILE $BASEDIR/$XMLWORK
+            fi
+        fi
+
+        if [[ $MD5JSON -eq 1 ]]; then
+            :
+        else
+            JSONFILE=$(find $BASEDIR/backup -type f -name "$JSONWORK-*" -printf '%T+ %f\n' | sort -r | cut -d' ' -f2 | sed -n '1 p')
+
+            if [[ -f $BASEDIR/backup/$JSONFILE && ! -z $JSONFILE ]]; then
+                cp -f $BASEDIR/backup/$JSONFILE $BASEDIR/$JSONWORK
+            fi
+        fi
+    fi
+}
 
 echolog() {
     echo "$@" | tee -a $BASEDIR/$LOGFILE
@@ -76,6 +180,8 @@ while true; do
         echolog "----------------------------------------------------"
         echolog ""
 
+        restoreBackup
+
         if [[ $DEBUG -eq 1 ]]; then
             alert "guide2go retry log"
         fi
@@ -89,6 +195,8 @@ while true; do
             echolog "[#] - error detected after loop threshold, exiting - [#]"
             echolog "--------------------------------------------------------"
 
+            restoreBackup
+
             alert "guide2go needs attention"
 
             exit 0
@@ -98,6 +206,9 @@ while true; do
         echolog "----------------------------"
         echolog "[#] - guide2go success - [#]"
         echolog "----------------------------"
+
+        addBackup
+        trimOldFiles
 
         if [[ $DEBUG -eq 1 ]]; then
             alert "guide2go debug log"
